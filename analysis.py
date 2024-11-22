f66c29af15f12d2c110ac03c2b2eb183ece8c32e
Anish <anish@LenovoAnish>
2024-11-22 15:15:19 +0530

    Done

--- analysis.py
+++ analysis.py
@@ -10,38 +10,25 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 def getReviews(rev_data):
 
-    # review_data= ""
-
-    # if __name__ == "__main__":
-    #     review_data=asyncio.run(sen())
-
-
-
-#converting into lowercase
     lower_dt=rev_data.lower()
 
     print(lower_dt)
 
-#data cleaning - removing punctuations
     cleaned_txt=lower_dt.translate(str.maketrans('','',string.punctuation))
 
 
 
-# #tokenization
     token_words=word_tokenize(cleaned_txt,"english")
     final_dt=[]
-#removing stop words
     for word in token_words:
         if word not in stopwords.words("english"):
             final_dt.append(word)
     
     emotion_list=set()
-    with open("D:\Projects\ProductConclusior\Conclusior\static\emotions.txt","r") as file:
+    with open("static\emotions.txt","r") as file:
         for line in file:
             clear_line=line.replace("'","").replace(",","").replace("\n","").strip()
-        #separating words and emotions
             word,emotion=clear_line.split(':')
-        # print(word,"=",emotion)
             if word in final_dt:
                 emotion_list.add(emotion)
 
--- app.py
+++ app.py
@@ -25,15 +25,12 @@ app = Flask(__name__)
 
 def init():
     global model,graph
-    # load the pre-trained Keras model
     model = load_model('sentiment_analysis.h5')
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    print("rendered")
     emotions=""
     emot=0.0
-    emot_unq=""
     sentiment=""
     if request.method=="POST":
         user_inp=request.form.get('user_input')
@@ -41,7 +38,7 @@ def home():
             emotions,emot,sentiment=sent_anly_prediction(user_inp)                    
         else:
             emotions="Invalid link"
-            emot=69
+            emot=18
 
         print(emotions,emot)
             
@@ -66,56 +63,60 @@ def sent_anly_prediction(prod_link):
 
         if not review_texts:
             print("No reviews found")
-            return None
-        rev_data="\n".join(review_texts)
-        emotions,emot=getReviews(rev_data)
+            rev_data="No reviews found"
+        else:
+            rev_data="\n".join(review_texts)
+            emotions,emot=getReviews(rev_data)
     finally:
         driver.quit()
     print("Function excuted")
     global model 
     try:
-        # Load the pre-trained Keras model
-        model = load_model('D:\Projects\ProductConclusior\Conclusior\sentiment_analysis.h5')
+        model = load_model('D:\MP\ProductConclusior\sentiment_analysis.h5')
         print("Model loaded successfully.")
     except Exception as e:
         print(f"Error loading model: {e}")
-        model = None  # Set model to None if loading fails # Declare model as global to access the loaded model
-    print(model.summary())
-    word_to_id = imdb.get_word_index()
-    vocab_size = 5000
-    word_to_id = {k: (v + 3) for k, v in word_to_id.items()}  # Shift indices for special tokens
-    word_to_id["<PAD>"] = 0
-    word_to_id["<START>"] = 1
-    word_to_id["<UNK>"] = 2
-    word_to_id["<UNUSED>"] = 3
-    word_to_id = {k: v for k, v in word_to_id.items() if v < vocab_size}
-    data = rev_data
-    sentiment = ''
-    max_review_length = 500
-    strip_special_chars = re.compile("[^A-Za-z0-9 ]+")
-    text = data.lower().replace("<br />", " ")
-    text = re.sub(strip_special_chars, "", text.lower())
+        model = None  
+    if(rev_data!="No reviews found"):
+        word_to_id = imdb.get_word_index()
+        vocab_size = 5000
+        word_to_id = {k: (v + 3) for k, v in word_to_id.items()}  # Shift indices for special tokens
+        word_to_id["<PAD>"] = 0
+        word_to_id["<START>"] = 1
+        word_to_id["<UNK>"] = 2
+        word_to_id["<UNUSED>"] = 3
+        word_to_id = {k: v for k, v in word_to_id.items() if v < vocab_size}
+        sentiment = ''
+        strip_special_chars = re.compile("[^A-Za-z0-9 ]+")
+        text = rev_data.lower().replace("<br />", " ")
+        text = re.sub(strip_special_chars, "", text.lower())
 
-    words = text.split()  # split string into a list
-    vocab_size = 5000  # Match this to the embedding layer's input_dim
-    x_test = [[word_to_id.get(word, 0) if word_to_id.get(word, 0) < vocab_size else 0 for word in words]]
-    x_test = pad_sequences(x_test, maxlen=500)  # Should be same which you used for training data
-    vector = np.array([x_test.flatten()])
+        words = text.split()  #list
+        vocab_size = 5000  # Match this to the embedding layer
+        x_test = [[word_to_id.get(word, 0) if word_to_id.get(word, 0) < vocab_size else 0 for word in words]]
+        x_test = pad_sequences(x_test, maxlen=500)  
+        vector = np.array([x_test.flatten()])
 
-        # No need to use graph in TensorFlow 2.x
-    if model is None:
-        return "Model not loaded.", 500  # Return an error response if model is None
-    probability = model.predict(array([vector][0]))[0][0]
-    class1 = (probability > 0.5).astype(int)  # 0 for Negative, 1 for Positive
+        if model is None:
+            return "Model not loaded.", 500  
+        probability = model.predict(array([vector][0]))[0][0]
 
-    if class1 == 0:
-        sentiment = 'Negative'
-        print(sentiment)
+        if probability < (0.4 - 0.1):
+            sentiment = 'Negative'
+            print(sentiment)
+        elif (0.4-0.1)<=probability<=(0.4+0.1):
+            sentiment = "Neutral"
+            print("Neutral")
+        else:
+            sentiment = 'Positive'
+            print(sentiment)
+
+        return (emotions,probability,sentiment)
     else:
-        sentiment = 'Positive'
-        print(sentiment)
-
-    return (emotions,probability,sentiment)
+        emotions=["No emotions"]
+        probability=0.0
+        sentiment="Neutral"
+        return (emotions,probability,sentiment)
 
 if __name__ == "__main__":
     init()
